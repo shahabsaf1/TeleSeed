550522ea4918ff820268317b2c715057dd7421b6
Shahab <safaeishahab@yahoo.com>
2016-08-07 15:55:55 +0430

    Update alexa.lua

--- plugins/alexa.lua
+++ plugins/alexa.lua
@@ -1,25 +1,11 @@
-﻿local function run(msg, matches)
-  local htp = http.request('http://api.magic-team.ir/alexas.php?site='..URL.escape(matches[1]))
-  local data = json:decode(htp)
-if not data then
-return 'Not Found !'
-else
-local lrank = data.lrank
-if not lrank then
-lrank = '---'
-end
-local lc = data.lcountry
-if not lc then
-lc = '---'
-end
-local text = '<b>Globally rank</b> : <code>'..data.grank..'</code>\n<b>Country</b> : <code>'..lc..'</code>\n<b>Country rank</b> : <code>'..lrank..'</code>\n<b>Information</b> : <code>'..data.info..'</code>'
- local text = text..'\n\n <a href="http://telegram.me/beatbot_team">BeatBotTeam</a> '
-send_api_msg(msg, get_receiver_api(msg), text, true, 'html')
-end
-end
+local function run(msg, matches)
+  local url = http.request('http://api.tarfandbazar.ir/alexa.php?url='..matches[1]..'')
+  local photo = 'http://api.tarfandbazar.ir/alexaphoto.php?url='..matches[1]
+  return url.."\n\n"..photo
+    end
 return {
-  usage = {'<code>!alexa [site]</code>\ncheck sites rank in alexa.com'},patterns = {
-    "^[!/#]alexa (.*)$"
-  },
-  run = run 
-} 
+   patterns = {
+"^[/#!]alexa (.*)$",
+   },
+   run = run
+}
